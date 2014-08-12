ad0cf5d404cd0492bfcefbd12cf1d75d5d961157
David Johnson <djohnson.m@gmail.com>
2014-08-12 09:37:49 -0500

    corrected mime type layout

--- src/Network/S3.hs
+++ src/Network/S3.hs
@@ -24,7 +24,7 @@ generateS3URL S3Keys{..} S3Request{..} = do
   time <- getExpirationTimeStamp secondsToExpire
   let url = case s3method of
               S3GET -> getURL bucketName objectName time
-              S3PUT -> putURL bucketName objectName mimeType time
+              S3PUT -> putURL bucketName objectName time mimeType 
       req = s3URL bucketName objectName publicKey time (sign secretKey url)
   return (S3URL req)
 
