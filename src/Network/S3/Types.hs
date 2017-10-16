e0edd26cdd6ab4d11d766e032c7a67eaf168ddba
William Casarin <jb55@jb55.com>
2017-10-15 20:49:21 -0700

    types: add S3Header newtype
    S3 headers must be lower case, and their value must be trimmed. This newtype
    inforces these invariants

--- src/Network/S3/Types.hs
+++ src/Network/S3/Types.hs
@@ -1,14 +1,27 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Network.S3.Types
     ( S3URL (..)
     , S3Keys (..)
     , S3Method (..)
     , S3Request (..)
+    , S3Header
+    , getS3Header
+    , s3Header
+    , s3HeaderBuilder
     ) where
 
-import           Data.ByteString.UTF8 (ByteString)
-import           GHC.Generics         (Generic)
+import           Data.ByteString.UTF8     (ByteString)
+import           GHC.Generics             (Generic)
+import           Data.Char8               (isSpace)
+import           Blaze.ByteString.Builder (Builder, fromByteString)
+import           Data.Monoid              (mconcat)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.CaseInsensitive  as CI
+
+newtype S3Header = S3Header { getS3Header :: (ByteString, ByteString) }
+  deriving (Generic, Show)
 
 newtype S3URL = S3URL {
       signedRequest :: ByteString -- ^ Generated URL
@@ -32,3 +45,17 @@ data S3Request = S3Request {
     , objectName      :: ByteString -- ^ Name of Amazon S3 File
     , secondsToExpire :: Integer -- ^ Number of seconds until expiration
     } deriving (Generic, Show)
+
+
+trim :: ByteString -> ByteString
+trim = BS.dropWhile isSpace . fst . BS.spanEnd isSpace
+
+s3Header :: ByteString -> ByteString -> S3Header
+s3Header header value = S3Header (lower, trimmed)
+  where
+    lower   = CI.foldCase header
+    trimmed = trim value
+
+s3HeaderBuilder :: S3Header -> Builder
+s3HeaderBuilder (S3Header (header,value)) =
+  mconcat [fromByteString header, ":", fromByteString value, "\n"]
