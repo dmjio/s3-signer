74e24e443677fb11c4cd54b6b96fb4be9554583e
William Casarin <jb55@jb55.com>
2017-10-18 18:40:32 -0700

    s3: update generateS3URL
    * Now only takes a secret key
    
    * No longer sources URL format from {get,put}URL methods, this information is
      generated from the bucket and S3Method information

--- src/Network/S3.hs
+++ src/Network/S3.hs
@@ -8,23 +8,17 @@ module Network.S3
     , module Network.S3.Types
     ) where
 
+import           Data.ByteString (ByteString)
+import           Data.Time.Clock (getCurrentTime)
 import           Network.S3.Sign
 import           Network.S3.Time
 import           Network.S3.Types
 import           Network.S3.URL
 
--- | Generates a cryptographically secure URL that expires within a
--- user defined period
---
--- See README for use cases and examples: <https://github.com/dmjio/s3-signer/blob/master/README.md>
-generateS3URL :: S3Keys -- ^ Amazon S3 Keys
-              -> S3Request  -- ^ Amazon S3 Request information
-              -> IO S3URL -- ^ Generated URL
-generateS3URL S3Keys{..} S3Request{..} = do
-  time <- getExpirationTimeStamp secondsToExpire
-  let url = case s3method of
-              S3GET -> getURL bucketName objectName time
-              S3PUT -> putURL bucketName objectName time mimeType 
-      req = s3URL bucketName objectName publicKey time (sign secretKey url)
-  return (S3URL req)
 
+generateS3URL :: ByteString -- ^ Amazon S3 SecretAccessKey
+              -> S3Request -- ^ Amazon S3 Request information
+              -> IO S3SignedRequest -- ^ Generated Request
+generateS3URL secretKey req = do
+  time <- getCurrentTime
+  return (sign secretKey req time)
