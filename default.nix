b371c76a419a1dc71f15e28712567ea6851962ea
David Johnson <djohnson.m@gmail.com>
2018-03-26 12:37:22 -0700

    Fix default.nix

--- default.nix
+++ default.nix
@@ -2,15 +2,19 @@
   let
    config = {
      packageOverrides = pkgs: with pkgs.haskell.lib; {
-       haskell.packages.${compiler} = pkgs.haskell.packages.${compiler}.override {
-          overrides = self: super: rec {
-            s3-signer = buildStrictly (self.callPackage ./s3-signer.nix { });
-          };
-        };
+       haskell = pkgs.haskell // {
+         packages = pkgs.haskell.packages // {
+            ${compiler} = pkgs.haskell.packages.${compiler}.override {
+              overrides = self: super: rec {
+                s3-signer = buildStrictly (self.callPackage ./s3-signer.nix { });
+              };
+           };
+         };
       };
    };
-   in
-     let
-       pkgs = import <nixpkgs> { inherit config; };
-       s3-signer = pkgs.haskell.packages.${compiler}.s3-signer;
-     in { inherit s3-signer; }
+};
+in
+  let
+    pkgs = import <nixpkgs> { inherit config; };
+    s3-signer = pkgs.haskell.packages.${compiler}.s3-signer;
+  in { inherit s3-signer; }
