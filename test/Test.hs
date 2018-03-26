5eb1a9b97168d79c828bd66ac68f7931f76df4a0
William Casarin <jb55@jb55.com>
2018-03-26 13:20:13 -0700

    test: assert that the signature is correct

--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 
 module Main where
 
@@ -6,12 +8,29 @@ import Data.ByteString (ByteString)
 import Data.Time.Calendar (fromGregorian)
 import Data.Time.Clock (UTCTime(..))
 import Network.S3
+import Control.Monad (unless)
 import Network.S3.Sign (sign)
+import System.Exit (exitFailure)
+
+expectedSig :: ByteString
+expectedSig = "f0e8bdb87c964420e857bd35b5d6ed310bd44f0170aba48dd91039c6036bdb41"
+
+testFail :: String -> IO ()
+testFail msg = do
+  print msg
+  exitFailure
+
+assertEqual :: (Show a, Eq a) => a -> a -> IO ()
+assertEqual got expected = do
+  let msg = "'" ++ show got ++ " does not match expected ''" ++ "'"
+  unless (got == expected) (testFail msg)
 
 main :: IO ()
 main = do
   let time = UTCTime (fromGregorian 2013 5 24) 0
-  print $ sign privKey (exampleRequest time) time
+      sr@S3SignedRequest{..} = sign privKey (exampleRequest time) time
+  assertEqual sigSignature expectedSig
+  print sr
 
 privKey :: ByteString
 privKey = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
