4793e874c2e3ec75fa8c263b6079188fe7f51a6d
William Casarin <jb55@jb55.com>
2018-03-26 13:27:16 -0700

    nix: update s3-signer.nix

--- s3-signer.nix
+++ s3-signer.nix
@@ -1,13 +1,16 @@
-{ mkDerivation, base, base64-bytestring, cryptohash, http-types
-, stdenv, time, utf8-string
+{ mkDerivation, base, base64-bytestring, blaze-builder, byteable
+, bytestring, case-insensitive, cryptohash, http-types, stdenv
+, time, utf8-string
 }:
 mkDerivation {
   pname = "s3-signer";
   version = "0.3.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base base64-bytestring cryptohash http-types time utf8-string
+    base base64-bytestring blaze-builder byteable bytestring
+    case-insensitive cryptohash http-types time utf8-string
   ];
+  testHaskellDepends = [ base blaze-builder bytestring time ];
   homepage = "https://github.com/dmjio/s3-signer";
   description = "Pre-signed Amazon S3 URLs";
   license = stdenv.lib.licenses.bsd3;
